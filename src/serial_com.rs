19d677dfe82eb0508277d3f4b93e74dd8ddede3e
Spencer Denton <spencerdent02@gmail.com>
2021-04-30 13:24:53 -0500

    serial communication working

--- src/serial_com.rs
+++ src/serial_com.rs
@@ -1,11 +1,11 @@
 use serial::{PortSettings, prelude::*, windows::COMPort};
-use std::io;
-use std::time::Duration;
+use std::{ffi::OsStr, io:: Write};
+
 
 pub fn init_serial() -> anyhow::Result<COMPort> {
-    let mut port = serial::open("COM4")?;
+    let mut port = serial::open(OsStr::new("COM5"))?;
     let settings = PortSettings {
-        baud_rate: serial::BaudRate::Baud1200,
+        baud_rate: serial::BaudRate::Baud115200,
         char_size: serial::CharSize::Bits8,
         parity: serial::Parity::ParityNone,
         stop_bits: serial::StopBits::Stop1,
@@ -17,6 +17,9 @@ pub fn init_serial() -> anyhow::Result<COMPort> {
     Ok(port)
 }
 
-// pub fn write_one_message (port: &COMPort) {
-    
-// }
+pub fn write_one_message(port: &mut COMPort, message: &[u8]) -> anyhow::Result<usize> {
+    let tx_len = port.write(message)?;
+    assert_eq!(tx_len, message.len());
+    port.flush()?;
+    Ok(tx_len)
+}
