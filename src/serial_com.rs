e4093c513e60e513cce5f8d6fdde63d007aa835d
Spencer Denton <spencerdent02@gmail.com>
2021-05-01 00:11:53 -0500

    serial working, but must init before data is sent

--- src/serial_com.rs
+++ src/serial_com.rs
@@ -1,18 +1,18 @@
 use serial::{PortSettings, prelude::*, windows::COMPort};
-use std::{ffi::OsStr, io:: Write};
+use std::{ffi::OsStr, io::{Read, Write}};
 
 
 pub fn init_serial() -> anyhow::Result<COMPort> {
     let mut port = serial::open(OsStr::new("COM5"))?;
-    let settings = PortSettings {
-        baud_rate: serial::BaudRate::Baud115200,
+    const SETTINGS: PortSettings = PortSettings {
+        baud_rate: serial::BaudRate::Baud1200,
         char_size: serial::CharSize::Bits8,
         parity: serial::Parity::ParityNone,
         stop_bits: serial::StopBits::Stop1,
         flow_control: serial::FlowControl::FlowNone,
     };
 
-    port.configure(&settings)?;
+    port.configure(&SETTINGS)?;
 
     Ok(port)
 }
@@ -22,4 +22,44 @@ pub fn write_one_message(port: &mut COMPort, message: &[u8]) -> anyhow::Result<u
     assert_eq!(tx_len, message.len());
     port.flush()?;
     Ok(tx_len)
+}
+
+pub fn read_one_message(port: &mut COMPort) -> (anyhow::Result<Vec<u8>>, i32) {
+    let mut error_count = 0;
+    let mut buf = Vec::new();
+    let mut iter = port.bytes();
+    let mut stx_semaphore: bool = false;
+
+    loop {
+        let next_byte = iter.next();
+
+        match next_byte {
+            Some(byte) => {
+                match byte {
+                    Ok(character) => {
+                        if character == 0x02 {
+                            stx_semaphore = true;
+                            continue;
+                        } else if character == 0x03 {
+                            if stx_semaphore == false {
+                                buf = Vec::new();
+                                continue;
+                            } else {
+                                return (Ok(buf), error_count);
+                            }
+                        } else if stx_semaphore == true {
+                            buf.push(character);
+                            continue;
+                        }
+                    },
+                    Err(_) => {
+                        error_count+=1;
+                        continue;
+                    },
+                };
+            },
+            None => break,
+        };
+    }
+    (Err(anyhow::Error::msg(error_count.to_string())), error_count)
 }
